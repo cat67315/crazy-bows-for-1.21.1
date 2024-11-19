53c50e9685c0b0cfc5a5eb0a53e407207d95c869
cat67315 <ezpurplecat@gmail.com>
2024-11-18 17:00:57 -0700

    Initial commit

---
+++ src/main/java/cat67315/crazybows/CrazyBows.java
@@ -0,0 +1,16 @@
+package cat67315.crazybows;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrazyBows implements ModInitializer {
+	public static final String MOD_ID = "crazy-bows";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/cat67315/crazybows/CrazybowsClient.java
@@ -0,0 +1,10 @@
+package cat67315.crazybows;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrazybowsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
